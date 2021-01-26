b18564f2abbcaaf23a9b4d447750ea3a851272c8
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-01-26 11:48:31 +0100

    Update author name

--- DE.pm
+++ DE.pm
@@ -658,13 +658,13 @@ L<https://github.com/tupinek/Mock-Person-DE>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © Michal Špaček 2013-2021
+ © Michal Josef Špaček 2013-2021
  BSD 2-Clause License
 
 =head1 VERSION
