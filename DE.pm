62e2f853779ccdc9c7ea6209919b08c2f835ae75
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-01-26 11:54:15 +0100

    Prepare for 0.07 version

--- DE.pm
+++ DE.pm
@@ -18,7 +18,7 @@ Readonly::Array our @EXPORT_OK => qw(first_male first_female middle_female
 our $TYPE = 'three';
 
 # Version.
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # First and middle male names.
 our @first_male = our @middle_male = qw(
@@ -671,6 +671,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
